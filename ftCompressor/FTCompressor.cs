0c7c3fccc3034ac104700a7f8a84090a5fc344a1
Morgan Estes <morgan@golftailor.com>
2013-11-13 10:47:57 -0600

    Updated the Decompressor to use StringBuilder, which is faster than basic string concatenation.

--- ftCompressor/FTCompressor.cs
+++ ftCompressor/FTCompressor.cs
@@ -34,14 +34,12 @@ namespace MorganEstes
 			}
 
 			// compress the string
-			int intStr1, intStr2;
 			StringBuilder strOut = new StringBuilder();
 
-			for (int i = 0; i < s.Length; i += 2)
-			{
+			for (int i = 0; i < s.Length; i += 2) {
 				// Char.ConvertToUtf32(string, pos) is JS string.charCodeAt(pos)
-				intStr1 = Char.ConvertToUtf32(s, i) * 256;
-				intStr2 = Char.ConvertToUtf32(s, i + 1);
+				int intStr1 = Char.ConvertToUtf32(s, i) * 256;
+				int intStr2 = Char.ConvertToUtf32(s, i + 1);
 
 				// Char.ConvertFromUtf32(int) is JS String.fromCharCode(int)
 				strOut.Append(Char.ConvertFromUtf32(intStr1 + intStr2));
@@ -54,21 +52,20 @@ namespace MorganEstes
 		public static string DecompressString(string s)
 		{
 			// If not prefixed with a snowman, just return the (already uncompressed) string
-			if (s [0] != Convert.ToChar (9731)) {
+			if (s[0] != Convert.ToChar(9731)) {
 				return s;
 			}
 
-			int m, n;
-			string strOut = "";
+			StringBuilder strOut = new StringBuilder();
 
 			for (int i = 1, l = s.Length; i < l; i++) {
-				n = Char.ConvertToUtf32 (s, i);
-				m = (int)Math.Floor ((decimal)n / 256);
+				int n = Char.ConvertToUtf32(s, i);
+				int m = (int)Math.Floor((decimal)n / 256);
 
-				strOut += (Char.ConvertFromUtf32 (m) + Char.ConvertFromUtf32 (n % 256));
+				strOut.Append(Char.ConvertFromUtf32(m) + Char.ConvertFromUtf32(n % 256));
 			}
 
-			return strOut;
+			return strOut.ToString();
 		}
 	}
 }
